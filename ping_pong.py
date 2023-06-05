059352cd6111dd4fdf0f44b80b00e9fe09c8797b
slepenkovart <105567185+slepenkovart@users.noreply.github.com>
2023-06-05 15:49:03 +0500

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -21,13 +21,13 @@ class Player(GameSprite):
         keys=key.get_pressed()
         if keys[K_UP] and self.rect.y>5:
             self.rect.y-=self.speed
-        if keys[K_DOWN] and self.rect.y<window_height-80:
+        if keys[K_DOWN] and self.rect.y<win_height-80:
             self.rect.y+=self.speed
     def update_r(self):
         keys=key.get_pressed()
         if keys[K_w] and self.rect.y>5:
             self.rect.y-=self.speed
-        if keys[K_s] and self.rect.y<window_height-80:
+        if keys[K_s] and self.rect.y<win_height-80:
             self.rect.y+=self.speed
 
 back=(200,255,255)
@@ -41,6 +41,17 @@ finish=False
 clock=time.Clock()
 FPS=60
 
+racket1 = Player('racket.png',30,200,50,150,4)
+racket2 = Player('racket.png',520,200,50,150,4)
+ball=GameSprite('tenis_ball.png',200,200,50,50,4)
+
+font.init()
+font=font.SysFont(None,35)
+lose1=font.render('Player 1 lose',True,(180,0,0))
+lose2=font.render('Player 2 lose',True,(180,0,0))
+
+speed_x=3
+speed_y=3
 
 
 while game:
@@ -49,5 +60,28 @@ while game:
             game=False
     if finish!=True:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x +=speed_x
+        ball.rect.y+=speed_y
+        
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+            speed_x*=-1
+            speed_y*=-1
+        if ball.rect.y > win_height-50 or ball.rect.y<0:
+            speed_y*=-1
+        if ball.rect.x <0:
+            finish = True
+            window.blit(lose1,(200,200))
+            game_over =True
+        if ball.rect.x >win_width:
+            finish = True
+            window.blit(lose2,(200,200))
+            game_over =True
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
     display.update()
     clock.tick(FPS)
